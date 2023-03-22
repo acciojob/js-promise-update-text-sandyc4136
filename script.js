c3ae5851d5a34ab4d1d9c91b4f3ebf45667ed195
Sandip Chhetry <sandyc4136@gmail.com>
2023-03-22 21:33:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const div=document.querySelector('#output');
+
+function promise(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000)
+	})
+}
+
+div.innerHTML=promise();
